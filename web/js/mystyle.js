a8d1630b99bd29029e51cbf3a2246b44f7fb3c97
rejey21 <84391791+rejey21@users.noreply.github.com>
2023-08-23 22:18:54 +0300

    Add files via upload

---
+++ web/js/mystyle.js
@@ -0,0 +1,9 @@
+function clock(){
+    var date = new Date(),
+           hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+           minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+           seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.getElementById('timer').innerHTML = hours + ':' + minutes + ':' + seconds;
+  }
+  setInterval(clock, 1000);
+  clock();
